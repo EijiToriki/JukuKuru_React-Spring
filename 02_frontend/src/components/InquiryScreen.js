09a706b4124e4972940f16b89ef0d6423b80fc2d
Eiji Toriki <ageForEngi@outlook.jp>
2023-11-11 23:10:00 +0900

    フロントエンド　照会画面 枠組み実装

--- 02_frontend/src/components/InquiryScreen.js
+++ 02_frontend/src/components/InquiryScreen.js
@@ -1,7 +1,89 @@
-import React from 'react'
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { Grid } from '@mui/material';
+import {classRoomDatesDummy, comeDatesDummy} from '../dummy_data/inquiryDummy';
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('12/26', 159, 6.0, 24, 4.0),
+  createData('12/27', 237, 9.0, 37, 4.3),
+  createData('12/28', 262, 16.0, 24, 6.0),
+  createData('12/29', 305, 3.7, 67, 4.3),
+  createData('12/30', 356, 16.0, 49, 3.9),
+];
+
+const comeDates = comeDatesDummy
+const classRoomDates = classRoomDatesDummy
 
 export default function InquiryScreen() {
+  const [classDates, setClassDates] = React.useState([])
+  const [komaList, setKomaList] = React.useState([])
+
+  React.useEffect(() => {
+    const dateList = []
+    const komaList = []
+    for(const classRoomDate of classRoomDates){
+      if(dateList.indexOf(classRoomDate.date) === -1){
+        dateList.push(classRoomDate.date)
+      }
+      if(komaList.indexOf(classRoomDate.class_time) === -1){
+        komaList.push(classRoomDate.class_time)
+      }
+    }
+
+    dateList.sort(function(a, b) {
+      return a.localeCompare(b)
+    })
+    komaList.sort(function(a, b) {
+      return a - b;
+    })
+
+    setClassDates(dateList)
+    setKomaList(komaList)
+  }, [])
+
+
   return (
-    <div>InquiryScreen</div>
-  )
+    <Grid container spacing={2} width="80%" margin="auto" marginTop="5%">
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>授業日</TableCell>
+              {
+                komaList.map((koma) => (
+                  <TableCell align="right" key={koma}>{koma}コマ目</TableCell>
+                ))
+              }
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {classDates.map((date) => (
+              <TableRow
+                key={date}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">{date}</TableCell>
+                {
+                  komaList.map((koma) => (
+                    // Todo : ここで生徒の来塾日情報から、三項演算子でしょりを分ける
+                    <TableCell align="right" key={koma}>{koma}コマ目</TableCell>
+                  ))
+                }
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Grid>
+  );
 }
---
+++ 02_frontend/src/dummy_data/inquiryDummy.js
@@ -0,0 +1,132 @@
+const comeDatesDummy = [
+  {
+      "id": 1,
+      "date": "2023-12-26",
+      "class_time": 1
+  },
+  {
+      "id": 2,
+      "date": "2023-12-26",
+      "class_time": 2
+  },
+  {
+      "id": 8,
+      "date": "2023-12-27",
+      "class_time": 3
+  },
+  {
+      "id": 9,
+      "date": "2023-12-27",
+      "class_time": 4
+  },
+  {
+      "id": 10,
+      "date": "2023-12-27",
+      "class_time": 5
+  }
+]
+
+const classRoomDatesDummy = [
+    {
+        "id": 1,
+        "date": "2023-12-26",
+        "class_time": 1
+    },
+    {
+        "id": 2,
+        "date": "2023-12-26",
+        "class_time": 2
+    },
+    {
+        "id": 3,
+        "date": "2023-12-26",
+        "class_time": 3
+    },
+    {
+        "id": 4,
+        "date": "2023-12-26",
+        "class_time": 4
+    },
+    {
+        "id": 5,
+        "date": "2023-12-26",
+        "class_time": 5
+    },
+    {
+        "id": 6,
+        "date": "2023-12-27",
+        "class_time": 1
+    },
+    {
+        "id": 7,
+        "date": "2023-12-27",
+        "class_time": 2
+    },
+    {
+        "id": 8,
+        "date": "2023-12-27",
+        "class_time": 3
+    },
+    {
+        "id": 9,
+        "date": "2023-12-27",
+        "class_time": 4
+    },
+    {
+        "id": 10,
+        "date": "2023-12-27",
+        "class_time": 5
+    },
+    {
+        "id": 11,
+        "date": "2023-12-28",
+        "class_time": 3
+    },
+    {
+        "id": 12,
+        "date": "2023-12-28",
+        "class_time": 4
+    },
+    {
+        "id": 13,
+        "date": "2023-12-28",
+        "class_time": 5
+    },
+    {
+        "id": 14,
+        "date": "2023-12-29",
+        "class_time": 1
+    },
+    {
+        "id": 15,
+        "date": "2023-12-29",
+        "class_time": 2
+    },
+    {
+        "id": 16,
+        "date": "2023-12-29",
+        "class_time": 3
+    },
+    {
+        "id": 17,
+        "date": "2023-12-29",
+        "class_time": 4
+    },
+    {
+        "id": 18,
+        "date": "2023-12-29",
+        "class_time": 5
+    },
+    {
+        "id": 19,
+        "date": "2023-12-30",
+        "class_time": 1
+    },
+    {
+        "id": 20,
+        "date": "2023-12-30",
+        "class_time": 2
+    }
+]
+
+export {comeDatesDummy, classRoomDatesDummy}
